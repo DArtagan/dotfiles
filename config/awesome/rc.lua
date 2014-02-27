2d3d36102dad0c81761d00f348b19385e2a97f6c
Willy Weiskopf <william@weiskopf.me>
2014-02-27 10:16:10 -0700

    Dropbox run on startup.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -454,3 +454,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 awful.util.spawn_with_shell("xrdb -merge $HOME/.Xresources")
 awful.util.spawn_with_shell("unagi &")
+awful.util.spawn_with_shell("dropboxd &")
