427b8fc0a2f203e25bd0060b6f11959a0cca9824
Willy Weiskopf <william@weiskopf.me>
2014-07-16 17:31:08 -0600

    Completion for b.

---
+++ autoload/b_completion.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+if [[ -n ${ZSH_VERSION-} ]]; then
+ 	autoload -U +X bashcompinit && bashcompinit
+fi
+
+_b()
+{
+	COMPREPLY=();
+	cur="${COMP_WORDS[COMP_CWORD]}";
+	if [[ "$COMP_CWORD" -eq "1" ]] ; then
+		opts=$(cut -f1 -d, ~/.b_bookmarks);
+		COMPREPLY=( $(compgen -W "$opts" -- $cur) );
+	fi
+        # include directories too, so you can replace cd with b
+	COMPREPLY=( ${COMPREPLY[@]} $(compgen -A directory "$cur"));
+}
+complete -F _b b
