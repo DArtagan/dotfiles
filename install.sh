a31583273c751be51370b53a40cc54668516aaca
Willy Weiskopf <william@weiskopf.me>
2014-08-18 18:19:06 -0600

    Get rid of fonts, let pacman or yaourt handle those.

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ olddir=~/.dotfiles_old             # old dotfiles backup directory
 backupdir=~/.backup/vim-backup     # directory for vim backup files
 swapdir=~/.backup/vim-swap         # directory for vim swap files
 # list of files/folders to symlink in homedir seperated by spaces
-files="antigen autoenv autoload bashrc Xresources oh-my-zsh pentadactylrc vimrc vim weechat zshenv zshrc zsh_aliases config/awesome config/terminator config/termite fonts/Inconsolata-Powerline.otf"
+files="antigen autoenv autoload bashrc Xresources oh-my-zsh pentadactylrc vimrc vim weechat zshenv zshrc zsh_aliases config/awesome config/terminator config/termite"
 
 ##########
 
