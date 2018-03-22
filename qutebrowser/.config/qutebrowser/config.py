cfd1a7964fa06dee81440c2f6978e3fc521fe9b4
Willy Weiskopf <william@weiskopf.me>
2018-03-21 22:37:37 -0400

    More qutebrowser configuration.

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,5 +1,15 @@
+# Reopen the previous session on program start
 c.auto_save.session = True
+# Open middle/ctrl+click tabs in the background
+c.tabs.background = True
+# When the last tab is closed, open the start page
 c.tabs.last_close = 'startpage'
 
 # Key bindings
 config.bind('t', 'set-cmd-text -s :open -t')
+
+# Search engines
+c.url.searchengines = {
+    'DEFAULT': 'https://www.google.com/search?hl=en&q={}',
+    'google': 'https://www.google.com/search?hl=en&q={}',
+}
