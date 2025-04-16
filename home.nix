39c434313171cefe8a8897f29dfb204c840e8b44
William Weiskopf <wweiskopf@ginkgobioworks.com>
2025-04-16 16:55:51 -0400

    Move fish into nix, and move possible packages to programs.

--- home.nix
+++ home.nix
@@ -1,35 +1,98 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   home = {
+    activation.configure-tide = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      ${pkgs.fish}/bin/fish -c "tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=No"
+    '';
+
     packages = with pkgs; [
-      awscli2
-      bottom
+      awsume
       curl
       devenv
       graphviz
-      home-manager
+      #fishPlugins.done
+      #fishPlugins.fzf-fish
+      #fishPlugins.tide
       fluxcd
-      fzf
-      jq
       magic-wormhole
       nix-output-monitor
       nixos-rebuild
+      nodejs # For vim CoC
       pgcli
       pstree
       rclone
-      ripgrep
       tree
-      zed-editor
+      uv
     ];
     shell.enableShellIntegration = true;
   };
 
   programs = {
+    awscli.enable = true;
+    bottom.enable = true;
     direnv = {
       enable = true;
       nix-direnv.enable = true;
     };
+    fish = {
+      enable = true;
+      functions = {
+        ua-drop-caches = ''
+          function ua-drop-caches --wraps='sudo paccache -rk3; sudo aura -Sc --noconfirm' --description 'alias ua-drop-caches sudo paccache -rk3; sudo aura -Sc --noconfirm'
+            sudo paccache -rk3; sudo aura -Sc --noconfirm $argv
+          end
+        '';
+        ua-update-all = ''
+          function ua-update-all --wraps=export\ TMPFILE=\"\$\(mktemp\)\"\;\ \\\n\ \ sudo\ true\;\ \\\n\ \ rate-mirrors\ --save=\$TMPFILE\ manjaro\ --max-delay=21600\ \\\n\ \ \ \ \&\&\ sudo\ mv\ /etc/pacman.d/mirrorlist\ /etc/pacman.d/mirrorlist-backup\ \\\n\ \ \ \ \&\&\ sudo\ mv\ \$TMPFILE\ /etc/pacman.d/mirrorlist\ \\\n\ \ \ \ \&\&\ ua-drop-caches\ \\\n\ \ \ \ \&\&\ sudo\ aura\ -Sy\ --noconfirm\ archlinux-keyring\ \\\n\ \ \ \ \&\&\ sudo\ aura\ -Syyu\ --noconfirm --description alias\ ua-update-all\ export\ TMPFILE=\"\$\(mktemp\)\"\;\ \\\n\ \ sudo\ true\;\ \\\n\ \ rate-mirrors\ --save=\$TMPFILE\ manjaro\ --max-delay=21600\ \\\n\ \ \ \ \&\&\ sudo\ mv\ /etc/pacman.d/mirrorlist\ /etc/pacman.d/mirrorlist-backup\ \\\n\ \ \ \ \&\&\ sudo\ mv\ \$TMPFILE\ /etc/pacman.d/mirrorlist\ \\\n\ \ \ \ \&\&\ ua-drop-caches\ \\\n\ \ \ \ \&\&\ sudo\ aura\ -Sy\ --noconfirm\ archlinux-keyring\ \\\n\ \ \ \ \&\&\ sudo\ aura\ -Syyu\ --noconfirm
+            export TMPFILE="$(mktemp)"; \
+            sudo true; \
+            rate-mirrors --save=$TMPFILE manjaro --max-delay=21600 \
+              && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+              && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
+              && ua-drop-caches \
+              && sudo aura -Sy --noconfirm archlinux-keyring \
+              && sudo aura -Syyu --noconfirm $argv
+          end
+        '';
+      };
+      plugins = [
+        {
+          name = "done";
+          inherit (pkgs.fishPlugins.done) src;
+        }
+        {
+          name = "fzf-fish";
+          inherit (pkgs.fishPlugins.fzf-fish) src;
+        }
+        {
+          name = "tide";
+          inherit (pkgs.fishPlugins.tide) src;
+        }
+        {
+          name = "fish-ai";
+          src = pkgs.fetchFromGitHub {
+            owner = "Realiserad";
+            repo = "fish-ai";
+            rev = "v1.7.0";
+            sha256 = "GnUBPkZZ0mfMUPnk62jxrAMGPFW8YxChhFBUBsdEwLA=";
+          };
+        }
+      ];
+      shellAliases = {
+        awsume = "source (which awsume.fish)";
+      };
+      shellInit = ''
+        	set -g -x PIP_REQUIRE_VIRTUALENV true
 
+                # Added by OrbStack: command-line tools and integration
+                # This won't be added again if you remove it.
+        	source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+      '';
+    };
+    fzf.enable = true;
+    home-manager.enable = true;
+    jq.enable = true;
+    ripgrep.enable = true;
     zed-editor = {
       enable = true;
       extensions = [
