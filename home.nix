99428062acd1c855586b8ff4c3e16b2832406f40
Willy Weiskopf <william@weiskopf.me>
2025-08-09 00:33:26 -0400

    Attempt moving zed into its own module.

--- home.nix
+++ home.nix
@@ -1,5 +1,9 @@
 { lib, pkgs, ... }:
 {
+  imports = [
+    ../modules/zed
+  ];
+
   home = {
     activation.configure-tide = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       ${pkgs.fish}/bin/fish -c "tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=No"
@@ -90,129 +94,5 @@
     home-manager.enable = true;
     jq.enable = true;
     ripgrep.enable = true;
-    zed-editor = {
-      enable = true;
-      extensions = [
-        "nix"
-        "toml"
-        "make"
-        "zed-legacy-themes"
-      ];
-
-      # everything inside of these brackets are Zed options - saved to settings.json
-      userSettings = {
-        features = {
-          edit_prediction_provider = "zed";
-        };
-
-        assistant = {
-          enabled = true;
-          version = "2";
-          default_open_ai_model = null;
-          #default_model = {
-          #  provider = "ollama";
-          #  model = "codegemma:7b";
-          #};
-        };
-
-        language_models = {
-          ollama = {
-            apiUrl = "http://localhost:11434";
-            availableModels = [
-              {
-                name = "codestral";
-                displayName = "codestral 22b";
-                maxTokens = 262144;
-              }
-              {
-                name = "qwen2.5-coder:32b";
-                displayName = "qwen2.5-coder:32b";
-                maxTokens = 32768;
-              }
-              {
-                name = "codegemma:7b";
-                displayName = "codegemma:7b";
-                maxTokens = 8192;
-              }
-            ];
-          };
-        };
-
-        hour_format = "hour24";
-        auto_update = false;
-
-        terminal = {
-          copy_on_select = true;
-          detect_venv = {
-            on = {
-              directories = [
-                ".env"
-                "env"
-                ".venv"
-                "venv"
-              ];
-              activate_script = "default";
-            };
-          };
-          font_family = "Hack Nerd Font";
-          font_size = null;
-          button = false;
-        };
-
-        lsp = {
-          nix = {
-            binary = {
-              path_lookup = true;
-            };
-          };
-          pyright = {
-            binary = {
-              path_lookup = true;
-            };
-          };
-        };
-
-        tab_size = 4;
-
-        languages = {
-          nix = {
-            tab_size = 2;
-          };
-        };
-
-        load_direnv = "shell_hook";
-        show_completions_on_input = true;
-        vim_mode = true;
-
-        theme = {
-          mode = "system";
-          light = "Zed Legacy: Solarized Light";
-          dark = "Zed Legacy: Solarized Dark";
-        };
-        show_whitespaces = "none";
-        indent_guides = {
-          enabled = true;
-          coloring = "indent_aware";
-        };
-        ui_font_size = 14;
-        buffer_font_size = 12;
-        #buffer_font_size = 14;
-        #buffer_font_family = "Hack Nerd Font";
-        #buffer_font_weight = 400;
-        cursor_blink = false;
-        relative_line_numbers = true;
-      };
-      userKeymaps = [
-        {
-          context = "Dock || Editor";
-          bindings = {
-            "ctrl-a h" = "workspace::ActivatePaneLeft";
-            "ctrl-a l" = "workspace::ActivatePaneRight";
-            "ctrl-a k" = "workspace::ActivatePaneUp";
-            "ctrl-a j" = "workspace::ActivatePaneDown";
-          };
-        }
-      ];
-    };
   };
 }
---
+++ modules/zed/default.nix
@@ -0,0 +1,129 @@
+{ }:
+{
+  programs = {
+    zed-editor = {
+      enable = true;
+      extensions = [
+        "nix"
+        "toml"
+        "make"
+        "zed-legacy-themes"
+      ];
+
+      # everything inside of these brackets are Zed options - saved to settings.json
+      userSettings = {
+        features = {
+          edit_prediction_provider = "zed";
+        };
+
+        assistant = {
+          enabled = true;
+          version = "2";
+          default_open_ai_model = null;
+          #default_model = {
+          #  provider = "ollama";
+          #  model = "codegemma:7b";
+          #};
+        };
+
+        language_models = {
+          ollama = {
+            apiUrl = "http://localhost:11434";
+            availableModels = [
+              {
+                name = "codestral";
+                displayName = "codestral 22b";
+                maxTokens = 262144;
+              }
+              {
+                name = "qwen2.5-coder:32b";
+                displayName = "qwen2.5-coder:32b";
+                maxTokens = 32768;
+              }
+              {
+                name = "codegemma:7b";
+                displayName = "codegemma:7b";
+                maxTokens = 8192;
+              }
+            ];
+          };
+        };
+
+        hour_format = "hour24";
+        auto_update = false;
+
+        terminal = {
+          copy_on_select = true;
+          detect_venv = {
+            on = {
+              directories = [
+                ".env"
+                "env"
+                ".venv"
+                "venv"
+              ];
+              activate_script = "default";
+            };
+          };
+          font_family = "Hack Nerd Font";
+          font_size = null;
+          button = false;
+        };
+
+        lsp = {
+          nix = {
+            binary = {
+              path_lookup = true;
+            };
+          };
+          pyright = {
+            binary = {
+              path_lookup = true;
+            };
+          };
+        };
+
+        tab_size = 4;
+
+        languages = {
+          nix = {
+            tab_size = 2;
+          };
+        };
+
+        load_direnv = "shell_hook";
+        show_completions_on_input = true;
+        vim_mode = true;
+
+        theme = {
+          mode = "system";
+          light = "Zed Legacy: Solarized Light";
+          dark = "Zed Legacy: Solarized Dark";
+        };
+        show_whitespaces = "none";
+        indent_guides = {
+          enabled = true;
+          coloring = "indent_aware";
+        };
+        ui_font_size = 14;
+        buffer_font_size = 12;
+        #buffer_font_size = 14;
+        #buffer_font_family = "Hack Nerd Font";
+        #buffer_font_weight = 400;
+        cursor_blink = false;
+        relative_line_numbers = true;
+      };
+      userKeymaps = [
+        {
+          context = "Dock || Editor";
+          bindings = {
+            "ctrl-a h" = "workspace::ActivatePaneLeft";
+            "ctrl-a l" = "workspace::ActivatePaneRight";
+            "ctrl-a k" = "workspace::ActivatePaneUp";
+            "ctrl-a j" = "workspace::ActivatePaneDown";
+          };
+        }
+      ];
+    };
+  };
+}
