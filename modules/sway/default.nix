ba5ca4b96bf53490000071164762c4e107c68b83
William Weiskopf <william@weiskopf.me>
2025-10-27 04:10:40 -0400

    Try and beat sway into usefulness.

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -1,15 +1,10 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    bemenu
     brightnessctl
-    i3status-rust
     grim
-    sway
     swayidle
     swaylock
-    swaynotificationcenter
-    wayland
     wl-clipboard
   ];
 
@@ -46,7 +41,48 @@
     pulse.enable = true;
   };
 
-  #home-manager.users.${config.username} = {
-
-  #};
+  # TODO: generalize this username
+  home-manager.users.will = {
+    programs = {
+      bemenu.enable = true;
+      i3status-rust = {
+        enable = true;
+      };
+    };
+    services.swaync.enable = true;
+    wayland.windowManager.sway = {
+      enable = true;
+      config = {
+        modifier = "Mod4";
+        menu = "bemenu-run";
+        terminal = "alacritty";
+        bars = [
+          {
+            fonts = {
+              names = [
+                "DejaVu Sans Mono"
+                "FontAwesome"
+              ];
+              size = 12;
+            };
+            position = "top";
+            command = "i3status-rs";
+            colors = {
+              separator = "#666666";
+              background = "#222222";
+              statusline = "#dddddd";
+              focusedWorkspace = {
+                border = "#0088CC";
+                background = "#0088CC";
+                text = "#ffffff";
+              };
+              #active_workspace #333333 #333333 #ffffff";
+              #inactive_workspace #333333 #333333 #888888";
+              #urgent_workspace #2f343a #900000 #ffffff";
+            };
+          }
+        ];
+      };
+    };
+  };
 }
