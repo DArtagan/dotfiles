cd10ac46b5fbecff7c58a0a9ff94e8b1fe3ebe30
William Weiskopf <william@weiskopf.me>
2025-11-14 01:12:58 -0500

    Autostart tailscale, allow connections from it on all ports.

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -5,4 +5,38 @@
     enable = true;
     openFirewall = true;
   };
+
+  networking.firewall = {
+    trustedInterfaces = [ "tailscale0" ];
+  };
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+    serviceConfig.Type = "oneshot";
+
+    # Make sure tailscale is running before trying to connect to tailscale
+    after = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wants = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+
+    # Have the job run this shell script
+    script = with pkgs; ''
+      # Wait for tailscaled to settle
+      sleep 2
+
+      # Check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # If so, then do nothing
+        exit 0
+      fi
+
+      ${tailscale}/bin/tailscale up
+    '';
+  };
 }
