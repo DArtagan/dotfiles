b5271801435240c1cf860ff2a5077ea5120df9f1
William Weiskopf <william@weiskopf.me>
2025-11-14 22:55:18 -0500

    Add speach AI server.

--- modules/ai-server/default.nix
+++ modules/ai-server/default.nix
@@ -10,7 +10,7 @@ _: {
       ports = [ "11434:11434" ];
       pull = "newer";
       volumes = [
-        "/mnt/mamba/ollama:/root/.ollama"
+        "/mnt/mamba/ai/ollama:/root/.ollama"
       ];
       extraOptions = [ "--device=nvidia.com/gpu=all" ];
     };
@@ -21,12 +21,33 @@ _: {
       autoStart = true;
       environment = {
         WEBUI_AUTH = "False";
+        AUDIO_STT_ENGINE = "openai";
+        AUDIO_STT_OPENAI_API_BASE_URL = "http://speaches:8000/v1";
+        AUDIO_STT_OPENAI_API_KEY = "does-not-matter";
+        AUDIO_STT_MODEL = "Systran/faster-distil-whisper-large-v3";
+        AUDIO_TTS_ENGINE = "openai";
+        AUDIO_TTS_OPENAI_API_BASE_URL = "http://speaches:8000/v1";
+        AUDIO_TTS_OPENAI_API_KEY = "does-not-matter";
+        AUDIO_TTS_MODEL = "speaches-ai/Kokoro-82M-v1.0-ONNX";
+        AUDIO_TTS_VOICE = "am_santa";
       };
       ports = [ "3000:8080" ];
       pull = "newer";
       volumes = [
-        "/mnt/mamba/open-webui:/app/backend/data"
+        "/mnt/mamba/ai/open-webui:/app/backend/data"
       ];
+    };
+
+    speaches = {
+      image = "ghcr.io/speaches-ai/speaches:latest-cuda";
+      autoRemoveOnStop = true;
+      autoStart = true;
+      ports = [ "11435:8000" ];
+      pull = "newer";
+      volumes = [
+        "/mnt/mamba/ai/speaches:/home/ubuntu/.cache/huggingface/hub"
+      ];
+      extraOptions = [ "--device=nvidia.com/gpu=all" ];
     };
   };
 }
